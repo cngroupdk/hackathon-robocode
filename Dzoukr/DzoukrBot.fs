187493cc67c47a56f0bafff689f94c45ad792056
Roman Provazník <dzoukr@dzoukr.cz>
2017-03-24 09:28:21 +0100

    Initial solution

---
+++ Dzoukr/DzoukrBot.fs
@@ -0,0 +1,24 @@
+﻿namespace CN.Dzoukr.Robocode
+
+open Robocode
+
+type DzoukrBot() =
+    inherit Robot()
+
+    override this.Run() = 
+        this.BodyColor <- System.Drawing.Color.Red
+        
+        while true do
+            this.TurnRight (this.Heading + 10.)
+            this.Ahead 100.
+            this.BodyColor <- System.Drawing.Color.LightSeaGreen
+        ()
+
+
+    override this.OnScannedRobot(args) =
+        this.TurnGunRight args.Bearing
+        this.Fire 10.
+        ()
+    
+    //override this.OnHitWall(args) =
+    //    args.
