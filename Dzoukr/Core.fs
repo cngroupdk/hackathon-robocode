8a2ae8c4bef8c7e03719f11641d73763ce320e6b
Roman Provazník <dzoukr@dzoukr.cz>
2017-03-24 16:47:16 +0100

    final fast-dead version

--- Dzoukr/Core.fs
+++ Dzoukr/Core.fs
@@ -28,11 +28,6 @@ type WallHitAlert =
     | CloseToTop
     | CloseToBottom
 
-//let isHeadingLeft heading = heading >= 225. && heading <= 315.
-//let isHeadingRight heading = heading >= 45. && heading <= 135.
-//let isHeadingTop heading = heading >= 315. || heading <= 45.
-//let isHeadingDown heading = heading >= 135. || heading <= 225.
-
 let (|WallHitAlert|_|) (config, position, heading) =
     if position.X <= config.WallLimit then //&& isHeadingLeft heading then
         Some CloseToLeft
@@ -44,73 +39,82 @@ let (|WallHitAlert|_|) (config, position, heading) =
         Some CloseToTop
     else None
 
-//type Movement =
-//    | Escape
-//    | Attack
-//    | Search
-//    | AvoidWall of WallHitAlert
-
 type Action =
     | Move of degrees:float * distance:float
     | Fire of degrees:float * power:float
     | Scan of degrees:float
 
-type Mode =
-    | Search
-    | Attack
-    | RunAway
+let normalize value =
+    if value < 0. then
+        360. - value
+    else if value > 360. then
+        value - 360.
+    else value
+    
 
 let randomWithCenter center =
     let center = int center
     let min,max = (center - 22),(center + 22)
-    System.Random().Next(min, max) |> float
+    System.Random().Next(min, max) |> float |> normalize
 
-let calculateNextAction (robot:Robot) config  =
+let calculateNextActions (robot:Robot) config  =
     let position = PositionOnField.create(robot)
     let moveAway = config.WallLimit * 1.5
     match config, position, robot.Heading with
-    | WallHitAlert(CloseToLeft) -> Move(randomWithCenter 90., moveAway)
-    | WallHitAlert(CloseToRight) -> Move(randomWithCenter 270., moveAway)
-    | WallHitAlert(CloseToTop) -> Move(randomWithCenter 180., moveAway)
-    | WallHitAlert(CloseToBottom) -> Move(randomWithCenter 0., moveAway)
-    | _ -> Move(robot.Heading, 100.)
+    | WallHitAlert(CloseToLeft) -> [Move(randomWithCenter 90., moveAway)]
+    | WallHitAlert(CloseToRight) -> [Move(randomWithCenter 270., moveAway)]
+    | WallHitAlert(CloseToTop) -> [Move(randomWithCenter 180., moveAway)]
+    | WallHitAlert(CloseToBottom) -> [Move(randomWithCenter 0., moveAway)]
+    | _ -> 
+        [
+            Move(robot.Heading, 100.); 
+            Scan(robot.RadarHeading + 45.)
+        ]
+
+let getPowerByDistance dist = 400. / dist
 
 let calculateScannedBotActions (robot:Robot) (args:ScannedRobotEvent)  =
     let heading = robot.Heading + args.Bearing
-    let firePower = 400. / args.Distance
+    let firePower = args.Distance |> getPowerByDistance
     [
         Fire(heading, firePower)
-        Move(heading, 50.)
         Scan(heading)
+        Move(heading, 50.)
     ]
 
-let effectiveTurn (robot:Robot) degrees =
-    match robot.Heading, degrees with
-    | h,d when h = d -> ()
-    | h,d ->
-        let abs = Math.Abs(h-d)
-        if abs >= 180. then
-            robot.TurnRight abs
-        else robot.TurnLeft abs
+let calculateHitBotActions (robot:Robot) (args:HitRobotEvent)  =
+    let heading = robot.Heading + args.Bearing
+    let firePower = 3.
+    [
+        Scan(heading)
+        Fire(heading, firePower)
+    ]
 
-let effectiveRadarTurn (robot:Robot) degrees =
-    match robot.RadarHeading, degrees with
-    | h,d when h = d -> ()
-    | h,d ->
-        let abs = Math.Abs(h-d)
-        if abs >= 180. then
-            robot.TurnRadarRight abs
-        else robot.TurnRadarLeft abs
+let calculateHitByBulletActions (robot:Robot) (args:HitByBulletEvent) =
+    let fireheading = robot.Heading + args.Bearing
+    [
+        Fire(fireheading, 0.1)
+    ]
 
+let effectiveTurn (heading:float) turnLeftF turnRighF newHeading =
+    match heading, newHeading with
+    | h,nh when h = nh -> ()
+    | h,nh ->
+        let diff = Math.Abs(h-nh)
+        if h > nh then
+            turnLeftF diff
+        else turnRighF diff
 
 let applyAction (robot:Robot) = function
     | Move(degrees, distance) -> 
-        degrees |> effectiveTurn robot
+        degrees |> effectiveTurn robot.Heading robot.TurnLeft robot.TurnRight
         robot.Ahead(distance)
     | Fire(degrees, power) ->
-        degrees |> effectiveTurn robot
-        robot.Fire(power)
+        if robot.Energy > power then
+            degrees |> effectiveTurn robot.Heading robot.TurnLeft robot.TurnRight
+            robot.Fire(power)
+        
     | Scan(degrees) ->
-        degrees |> effectiveRadarTurn robot
+        degrees |> effectiveTurn robot.RadarHeading robot.TurnRadarLeft robot.TurnRadarRight
 
 let applyActions (robot:Robot) = List.iter (applyAction robot)
--- Dzoukr/DzoukrBot.fs
+++ Dzoukr/DzoukrBot.fs
@@ -12,43 +12,31 @@ type DzoukrBot() =
         Color = System.Drawing.Color.HotPink
         WallLimit = 50.
     }
-
-    //let escapeMove = walllimit * 0.5
-    //let panicEscapeMove = 200.
-    //let attackMove = 100.
-    
-    //let getHeading (me:DzoukrBot) bearing = me.Heading - me.GunHeading + bearing
-    //let getFirePower (enemyDistance:float) = 400. / enemyDistance
-
-    //member this.ShootToEnemy enDistance enBearing =
-    //    let firePower = enDistance |> getFirePower
-    //    if this.Energy > firePower then
-    //        enBearing |> getHeading this |> this.TurnRight
-    //        firePower |> this.Fire
     
     override this.Run() = 
         this.BodyColor <- config.Color
         
         while true do
             config 
-            |> calculateNextAction this
-            |> applyAction this
+            |> calculateNextActions this
+            |> applyActions this
         ()
 
     override this.OnScannedRobot(args) =
         args 
         |> calculateScannedBotActions this 
         |> applyActions this
-        //this.ShootToEnemy args.Distance args.Bearing
-        //attackMove |> this.Ahead
         ()
     
     override this.OnHitRobot(args) =
-        //this.ShootToEnemy 0.1 args.Bearing
+        args 
+        |> calculateHitBotActions this 
+        |> applyActions this
         ()
     
     override this.OnHitByBullet(args) =
-        //this.ShootToEnemy 1. args.Bearing
-        //attackMove |> this.Ahead
+        args 
+        |> calculateHitByBulletActions this 
+        |> applyActions this
         ()
    
