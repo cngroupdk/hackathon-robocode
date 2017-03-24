053783366db90556f5a952a7c9c10c1449d9d5e9
dusanp <dusan.pilka@gmail.com>
2017-03-24 16:30:44 +0100

    simplification attempt

--- DPI/DPI_bot/DPIRobot.cs
+++ DPI/DPI_bot/DPIRobot.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.Remoting.Services;
 using Robocode;
 
 namespace DPI_bot
 {
     class DPIRobot:Robot
     {
+        private bool _isrunning = false;
         Random rand = new Random();
         public override void Run()
         {
             BodyColor = Color.Blue;
+            ScanColor = Color.Blue;
+            BulletColor = Color.White;
+            GunColor = Color.Blue;
+            RadarColor = Color.Blue;
+            IsAdjustRadarForGunTurn = false;
+            IsAdjustGunForRobotTurn = true;
+            IsAdjustRadarForRobotTurn = false;
+
             var r = new Random();
             while (true)
             {
-                TurnRadarRight(360);
+                TurnGunRight(90);
+                TurnGunRight(90);
+                TurnGunRight(90);
+                TurnGunRight(90);
+                Console.WriteLine("Basic Scan");
             }
         }
 
@@ -23,23 +37,107 @@ namespace DPI_bot
             get { return Energy < 20; }
         }
 
-        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        void TryFire(double distance)
         {
-            //TurnRight(evnt.Bearing);
-            TurnGunRight(evnt.Bearing-GunBearing);
-            if (GunHeat<0.1 && !EmergencyMode)
+            Console.WriteLine("Firing");
+            if (GunHeat < 0.1 && !EmergencyMode)
             {
-                if (evnt.Distance < 300 && evnt.Velocity<0.5)
+                if (distance < 150)
                 {
                     Fire(Rules.MAX_BULLET_POWER);
+                }
+                else if (distance <250)
+                {
+                    Fire(2);
+                }
+                else if (distance < 350)
+                {
+                    Fire(1);
                 }
                 else
                 {
                     Fire(Rules.MIN_BULLET_POWER);
                 }
             }
-            TurnRight(rand.Next(0,360)-180);
-            AheadNotIntoWall(150);
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent evnt)
+        {
+            Console.WriteLine("Scanned");
+
+            Func<bool> isAimingAtMe =
+                () =>
+                {
+                    var angleToMe = Math.Abs(Heading + evnt.Bearing - evnt.Heading);
+                    return angleToMe < 200 && angleToMe > 160;
+                };
+
+            if (isAimingAtMe()&&evnt.Distance<250&&evnt.Velocity>1)
+            {
+                Console.WriteLine("Escaping ram");
+                Dodge(evnt.Bearing);
+            }
+            else if (isAimingAtMe() || evnt.Velocity < 1 || evnt.Distance<250)
+            {
+                TurnGunRight(evnt.Bearing - GunBearing);
+                Console.WriteLine("shooting slow target");
+                TryFire(evnt.Distance);
+            }
+            else
+            {
+                Console.WriteLine("nothing to do");
+                Dodge(rand.Next(0,355)-180);
+            }
+            Console.WriteLine("Scanned end");
+            Scan();
+        }
+
+
+
+        public override void OnHitRobot(HitRobotEvent evnt)
+        {
+            Console.WriteLine("been hit by robot");
+            Dodge(evnt.Bearing);
+        }
+
+        void TurnAndMove(double angle, double distance)
+        {
+            Console.WriteLine("TURN AND MOVE angle: " + angle);
+            if (angle > 180)
+            {
+                TurnRight(angle - 180);
+                Ahead(-distance);
+            }
+            else if (angle < -180)
+            {
+                TurnRight(angle + 180);
+                Ahead(-distance);
+            }
+            else
+            {
+                TurnRight(angle);
+                Ahead(distance);
+            }
+        }
+
+        private void Dodge(double bearing)
+        {
+            TurnAndMove(bearing - 90, rand.Next(0, 1) == 0 ? 250 : -250);
+        }
+
+        public override void OnHitByBullet(HitByBulletEvent evnt)
+        {
+            Console.WriteLine("been hit by bullet");
+            Dodge(evnt.Bearing);
+        }
+
+        private double RadarBearing
+        {
+            get
+            {
+                var res = RadarHeading - Heading;
+                return res;
+            }
         }
 
         private double GunBearing
@@ -49,19 +147,6 @@ namespace DPI_bot
                 var res = GunHeading - Heading;
                 return res;
             }
-        }
-
-
-        private void AheadNotIntoWall(double distance)
-        {
-            if ((Heading>0 && Heading<180 && X>BattleFieldWidth-distance)
-            || (Heading > 90 && Heading < 270 && Y < distance)
-            || (Heading > 180 && X < distance)
-            || ((Heading > 270 || Heading < 90) && Y > BattleFieldHeight - distance))
-            {
-                TurnRight(180);
-            }
-            Ahead(distance);
         }
     }
 }
